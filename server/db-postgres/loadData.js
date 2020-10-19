ad2dfe15bff22cc26d070bd32369f5279dd35445
Emma Li <emma.m.li.98@gmail.com>
2020-10-19 11:58:24 -0700

    Creates loading script for Postgres

--- server/db-postgres/loadData.js
+++ server/db-postgres/loadData.js
@@ -1,16 +1,78 @@
 const db = require('./index.js');
 
-// TODO: Load .txt files into database using COPY
-// TODO: Create indexes, add foreign keys
+let tables = [
+  'hotels',
+  'hotel_languages',
+  'hotel_amenities',
+  'hotel_features',
+  'hotel_room_types',
+  'images'
+];
 
-// Testing Connection; remove later
-db.query(`SELECT * FROM languages WHERE language='English'`)
-  .then((result) => {
-    console.log(result.rows);
-  })
-  .then(() => {
-    db.end().then(() => { console.log('seeding has ended')});
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+let columns = [
+  ['hotel_name',
+  'description',
+  'number_of_reviews',
+  'rank',
+  'overall_rating',
+  'location_rating',
+  'cleanliness_rating',
+  'service_rating',
+  'value_rating',
+  'hotel_class',
+  'hotel_style',
+  'hotel_website'],
+  ['hotel_id', 'lang_id'],
+  ['hotel_id', 'amenity_id'],
+  ['hotel_id', 'feature_id'],
+  ['hotel_id', 'room_type_id'],
+  ['hotel_id', 'image_url']
+];
+
+let filenames = [
+  'hotels.csv',
+  'hotelLanguages.csv',
+  'hotelAmenities.csv',
+  'hotelFeatures.csv',
+  'hotelRoomTypes.csv',
+  'hotelImages.csv'
+];
+
+let table, columnList, filename;
+let queries = [];
+
+for (var i = 0; i < tables.length; i++) {
+  table = tables[i];
+  columnList = columns[i].join(', ');
+  filename = filenames[i];
+  let query = `COPY ${table}(${columnList})
+    FROM '${__dirname}/seedData/${filename}'
+    DELIMITER '|' CSV HEADER`;
+
+  queries.push(db.query(query));
+}
+
+// Add indexes and foreign keys after insertion
+queries.push(db.query('ALTER TABLE hotels ADD COLUMN hotel_id SERIAL PRIMARY KEY'));
+queries.push(db.query(`ALTER TABLE images ADD CONSTRAINT hotel_link
+  FOREIGN KEY(hotel_id) REFERENCES hotels(hotel_id)`)
+);
+
+for(var i = 1; i < 5; i++) {
+  queries.push(db.query(`ALTER TABLE ${tables[i]} ADD CONSTRAINT hotel_link
+    FOREIGN KEY(hotel_id) REFERENCES hotels(hotel_id)`)
+  );
+  queries.push(db.query(`ALTER TABLE ${tables[i]} ADD CONSTRAINT other_link
+    FOREIGN KEY(${columns[i][1]}) REFERENCES ${tables[i].slice(6)}(id)`)
+  );
+}
+
+// Execute all queries
+Promise.all(queries)
+.then((results) => {
+  console.log(results);
+  db.end().then(() => console.log('Seeding is done.'))
+})
+.catch((err) => {
+  console.error(err);
+})
