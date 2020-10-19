5bae0e35a499ad066f91f7fcc6454b407e47bf52
Emma Li <emma.m.li.98@gmail.com>
2020-10-19 11:56:08 -0700

    Adds formatting to Postgres data gen script for CSV files

--- server/db-postgres/generateData.js
+++ server/db-postgres/generateData.js
@@ -5,8 +5,11 @@ const description = require('../../database/descriptionGenerator.js');
 
 const hotelsFile = fs.createWriteStream(path.join('.', 'server', 'db-postgres', 'seedData', 'hotels.csv'));
 (async() => {
-  for (let i = 1; i <= 10000000; i++) {
-    // Order: hotelName, desc, reviews, rank, overall, ratings, class, style, website
+  let header = `hotel_name|description|number_of_reviews|rank|overall_rating|location_rating|cleanliness_rating|`;
+  header += `service_rating|value_rating|hotel_class|hotel_style|hotel_website\n`;
+  hotelsFile.write(header);
+
+  for (let i = 1; i <= 10; i++) {
     let row = '';
     row += 'hotel' + i + '|';
     row += description() + '|';
@@ -27,7 +30,10 @@ const hotelsFile = fs.createWriteStream(path.join('.', 'server', 'db-postgres', 
 
 const langFile = fs.createWriteStream(path.join('.', 'server', 'db-postgres', 'seedData', 'hotelLanguages.csv'));
 (async() => {
-  for (let i = 1; i <= 10000000; i++) {
+  let header = `hotel_id|lang_id\n`;
+  langFile.write(header);
+
+  for (let i = 1; i <= 10; i++) {
     for (var j = 1; j <= 8; j++) {
       if (faker.random.boolean() && !langFile.write(`${i}|${j}\n`)) {
         await new Promise(resolve => langFile.once('drain', resolve));
@@ -38,7 +44,10 @@ const langFile = fs.createWriteStream(path.join('.', 'server', 'db-postgres', 's
 
 const featureFile = fs.createWriteStream(path.join('.', 'server', 'db-postgres', 'seedData', 'hotelFeatures.csv'));
 (async() => {
-  for (let i = 1; i <= 10000000; i++) {
+  let header = `hotel_id|feature_id\n`;
+  featureFile.write(header);
+
+  for (let i = 1; i <= 10; i++) {
     for (var j = 1; j <= 19; j++) {
       if (faker.random.boolean() && !featureFile.write(`${i}|${j}\n`)) {
         await new Promise(resolve => featureFile.once('drain', resolve));
@@ -49,7 +58,10 @@ const featureFile = fs.createWriteStream(path.join('.', 'server', 'db-postgres',
 
 const amenFile = fs.createWriteStream(path.join('.', 'server', 'db-postgres', 'seedData', 'hotelAmenities.csv'));
 (async() => {
-  for (let i = 1; i <= 10000000; i++) {
+  let header = `hotel_id|amenity_id\n`;
+  amenFile.write(header);
+
+  for (let i = 1; i <= 10; i++) {
     for (var j = 1; j <= 41; j++) {
       if (faker.random.boolean() && !amenFile.write(`${i}|${j}\n`)) {
         await new Promise(resolve => amenFile.once('drain', resolve));
@@ -60,7 +72,10 @@ const amenFile = fs.createWriteStream(path.join('.', 'server', 'db-postgres', 's
 
 const roomFile = fs.createWriteStream(path.join('.', 'server', 'db-postgres', 'seedData', 'hotelRoomTypes.csv'));
 (async() => {
-  for (let i = 1; i <= 10000000; i++) {
+  let header = `hotel_id|room_type_id\n`;
+  roomFile.write(header);
+
+  for (let i = 1; i <= 10; i++) {
     for (var j = 1; j <= 4; j++) {
       if (faker.random.boolean() && !roomFile.write(`${i}|${j}\n`)) {
         await new Promise(resolve => roomFile.once('drain', resolve));
@@ -71,10 +86,13 @@ const roomFile = fs.createWriteStream(path.join('.', 'server', 'db-postgres', 's
 
 const imagesFile = fs.createWriteStream(path.join('.', 'server', 'db-postgres', 'seedData', 'hotelImages.csv'));
 (async() => {
-  for (let i = 1; i <= 10000000; i++) {
+  let header = `hotel_id|image_url\n`;
+  imagesFile.write(header);
+
+  for (let i = 1; i <= 10; i++) {
     for (var j = 0; j < 8; j++) {
       let url = `https://tripadcoba-about.s3.us-east-2.amazonaws.com/image${faker.random.number({min: 1, max: 100})}.jpg`;
-      if (faker.random.boolean() && !imagesFile.write(`${i}|${url}\n`)) {
+      if (!imagesFile.write(`${i}|${url}\n`)) {
         await new Promise(resolve => imagesFile.once('drain', resolve));
       }
     }
